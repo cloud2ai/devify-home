7b565ba007e36cf5ea44941e1180517390f00b88
Ray Sun <xiaoquqi@gmail.com>
2026-06-11 22:46:04 +0800

    chore: add Google tag to docs site

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -75,7 +75,14 @@ export default defineConfig({
 
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }],
-    ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1.0' }]
+    ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1.0' }],
+    ['script', { async: '', src: 'https://www.googletagmanager.com/gtag/js?id=G-TJWJHQH4M9' }],
+    ['script', {}, `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-TJWJHQH4M9');
+    `]
   ],
 
   vite: {
